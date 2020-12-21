9901a23920be860cb3b065d23e1344111cff3480
Tsumifa <tsumifa@gmail.com>
2020-12-21 21:14:32 +0100

    work in progress

--- src/interface/root.py
+++ src/interface/root.py
@@ -4,9 +4,37 @@
 
 try:
     
-    import tkinter
+    import tkinter as tk
 
 except Exception as e:
     raise e
 
 # == LOGIC PART == #
+
+
+class Root:
+
+    def __init__(self, window_size, window_title, window_background, window_icon_path):
+        
+        '''
+            Create window
+        '''
+        self.WINDOW = tk.Tk()
+
+        '''
+            Settings of the window
+        '''
+        self.GEOMETRY = window_size
+        self.TITLE = window_title
+        self.BACKGROUND = window_background
+        self.ICON = window_icon_path
+
+
+    def initialize_window(self):
+        self.WINDOW.title(self.TITLE)
+        self.WINDOW.geometry(self.GEOMETRY)
+        self.WINDOW.iconbitmap(self.ICON)
+        self.WINDOW.configure(background=self.BACKGROUND)
+
+    def create_widget(self):
+        pass
---
+++ src/interface/views/options.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# == IMPORTATIONS == #
+
+try:
+    #- Librairies
+    import tkinter as tk
+
+except Exception as e:
+    raise e
+
+# == LOGIC PART == #
+
+
+class Options_Window:
+
+    def __init__(self):
+        
+        self.window = tk.tk()
+
+    def create_widget(self):
+        pass
